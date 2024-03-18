961d19b5e27df5168e1e6891997f4b81ade581fe
kimchanyo <chanyokr@gmail.com>
2024-03-18 22:15:39 +0900

    chapter 7

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import OrderContext, { OrderContextProvider } from "./context/OrderContext";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <OrderContextProvider>
+      <App />
+    </OrderContextProvider>
   </React.StrictMode>
 );
 
